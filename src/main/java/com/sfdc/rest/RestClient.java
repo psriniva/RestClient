234f68c0284a4cba9c2bea32bdda169eb2aea205
Prashant Srinivasan <psrinivasan@salesforce.com>
2012-10-20 15:13:00 -0700

    Initial Commit

---
+++ src/main/java/com/sfdc/rest/RestClient.java
@@ -0,0 +1,11 @@
+package com.sfdc.rest;
+
+import com.sfdc.http.client.NingAsyncHttpClientImpl;
+
+/**
+ * @author psrinivasan
+ *         Date: 10/19/12
+ *         Time: 8:33 PM
+ */
+public class RestClient extends NingAsyncHttpClientImpl {
+}
